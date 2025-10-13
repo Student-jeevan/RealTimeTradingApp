2439dc28717453518b152f62c4dd357b8dc71fbd
jeevansingh <jeevanthitrya@gmail.com>
2025-10-13 11:20:37 +0530

    payment Details service added

---
+++ TradingApp/src/main/java/com/jeevan/TradingApp/controller/PaymentDetailsController.java
@@ -0,0 +1,43 @@
+package com.jeevan.TradingApp.controller;
+
+import com.jeevan.TradingApp.modal.PaymentDetails;
+import com.jeevan.TradingApp.modal.User;
+import com.jeevan.TradingApp.service.PaymentDetailsService;
+import com.jeevan.TradingApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PaymentDetailsController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PaymentDetailsService paymentDetailsService;
+
+    @PostMapping("/payment-details")
+
+    public ResponseEntity<PaymentDetails> addPaymentDetails(@RequestBody PaymentDetails paymentDetailsRequest , @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        PaymentDetails paymentDetails = paymentDetailsService.addPaymentDetails(
+                paymentDetailsRequest.getAccountNumber() ,
+                paymentDetailsRequest.getAccountHolderName(),
+                paymentDetailsRequest.getIfsc(),
+                paymentDetailsRequest.getBankName(),
+                user
+        );
+        return new ResponseEntity<>(paymentDetails, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/payment-details")
+    public ResponseEntity<PaymentDetails> getUsersPaymentDetails(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        PaymentDetails paymentDetails = paymentDetailsService.getUsersPaymentDetails(user);
+        return new ResponseEntity<>(paymentDetails , HttpStatus.CREATED);
+    }
+
+}
---
+++ TradingApp/src/main/java/com/jeevan/TradingApp/service/PaymentDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.jeevan.TradingApp.service;
+
+import com.jeevan.TradingApp.modal.PaymentDetails;
+import com.jeevan.TradingApp.modal.User;
+import com.jeevan.TradingApp.repository.PaymentDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentDetailsServiceImpl implements  PaymentDetailsService {
+
+    @Autowired
+    private PaymentDetailsRepository paymentDetailsRepository;
+    @Override
+    public PaymentDetails addPaymentDetails(String accountNumber, String accountHolderName, String ifsc, String bankName, User user) {
+        PaymentDetails paymentDetails = new PaymentDetails();
+
+        paymentDetails.setAccountNumber(accountNumber);
+        paymentDetails.setBankName(bankName);
+        paymentDetails.setUser(user);
+        paymentDetails.setIfsc(ifsc);
+        paymentDetails.setAccountHolderName(accountHolderName);
+        return paymentDetailsRepository.save(paymentDetails);
+    }
+    @Override
+    public PaymentDetails getUsersPaymentDetails(User user) {
+
+        return paymentDetailsRepository.findByUserId(user.getId());
+    }
+}
