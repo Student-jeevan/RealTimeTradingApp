6ff3d6a4bfe32fe8f6a99e1f19e3843ca75d1390
jeevansingh <jeevanthitrya@gmail.com>
2025-10-30 22:50:11 +0530

    sidebar added:

--- frontend/src/pages/Navbar/Navbar.jsx
+++ frontend/src/pages/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 import { Button } from '@/components/ui/button';
 import {
   Sheet,
@@ -9,6 +10,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { DragHandleHorizontalIcon} from "@radix-ui/react-icons"
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
 const Navbar = ()=>{
     return (
         <div className='px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 right-0 flex justify-between items-center'>
@@ -20,14 +22,22 @@ const Navbar = ()=>{
                             <DragHandleHorizontalIcon className ='h-7 w-7' />
                         </Button>
                     </SheetTrigger>
-                    <SheetContent side="left">
+                    <SheetContent className="w-72 border-r-0 flex flex-col justify-center" side="left">
                         <SheetHeader>
-                        <SheetTitle>Are you absolutely sure?</SheetTitle>
-                        <SheetDescription>
-                            This action cannot be undone. This will permanently delete your account
-                            and remove your data from our servers.
-                        </SheetDescription>
+                        <SheetTitle>
+                            <div className="text-2xl flex justify-center items-center gap-1">
+                                <Avatar>
+                                    <AvatarImage src="/crypto_logo.png"  alt="Crypto App Logo - CryptEx Trade"  />
+                                </Avatar>
+                                <div>
+                                    <span className="font-bold text-orange-700">Crypto</span>
+                                    <span>Trading</span>
+                                </div>
+                            </div>
+                        </SheetTitle>
+                        
                         </SheetHeader>
+                        <Sidebar/>
                     </SheetContent>
                 </Sheet>
             </div>
---
+++ frontend/src/pages/Navbar/Sidebar.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button';
+import { BookmarkIcon, CreditCardIcon, HomeIcon, LandmarkIcon, Wallet, WalletIcon } from 'lucide-react';
+import { SheetClose } from '@/components/ui/sheet';
+import React from 'react'
+import { ActivityLogIcon, DashboardIcon, ExitIcon, PersonIcon } from '@radix-ui/react-icons';
+const menu=[
+    {name:"Home" , path:"/", icon:<HomeIcon className='h-6 w-6'/>},
+    {name:"Portfolio" , path:"/portfolio" , icon:<DashboardIcon className="h-6 w-6"/>},
+    {name:"Watchlist" , path:"/watchlist" , icon:<BookmarkIcon className="h-6 w-6"/>},
+    {name:"Activity" , path:"/activity" , icon:<ActivityLogIcon className="h-6 w-6"/>},
+    {name:"Wallet" , path:"/wallet" , icon:<WalletIcon className="h-6 w-6"/>},
+
+    {name:"Payment Details" , path:"/payment-details" , icon:<LandmarkIcon className="h-6 w-6"/>},
+
+    {name:"Withdrawal" , path:"/Withdrawal" , icon:<CreditCardIcon className="h-6 w-6"/>},
+    {name:"Profile" , path:"/profile" , icon:<PersonIcon className="h-6 w-6"/>},
+    {name:"Logout" , path:"/" , icon:<ExitIcon className="h-6 w-6"/>}
+]
+const Sidebar = () =>{
+    return (
+        <div className='mt-10 space-y-5'>
+            {menu.map((item)=>(
+                <div key={item.name}>
+                    <SheetClose className='w-full'>
+                    <Button variant="outline" className='flex items-center gap-5 py-6 w-full'>
+                        <span className='w-8'>{item.icon}</span>
+                        <p>{item.name}</p>
+                    </Button>
+                </SheetClose>
+                </div>
+            ))}
+        </div>
+    )
+}
+export default Sidebar;
