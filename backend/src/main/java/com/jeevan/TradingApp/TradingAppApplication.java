afb6f9dfed7da694fb9de80202d7523f7b1c74cd
jeevansingh <jeevanthitrya@gmail.com>
2025-12-14 22:31:45 +0530

    jar and war files added

--- backend/src/main/java/com/jeevan/TradingApp/TradingAppApplication.java
+++ backend/src/main/java/com/jeevan/TradingApp/TradingAppApplication.java
@@ -3,12 +3,19 @@ package com.jeevan.TradingApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
 @EnableCaching
-public class TradingAppApplication {
+public class TradingAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TradingAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingAppApplication.class, args);
 	}
-
 }
+
